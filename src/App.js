32560df6b89c4e828c95870bae0c773608e3469b
leehyemie <80510889+leehyemie@users.noreply.github.com>
2022-06-03 11:22:09 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ function App(){
     return (
     <div>
         <div>
-            <SearchContainer />   
-                         
+            <SearchContainer />                            
         </div>
         <div className="App">
             <MaterialTable />
